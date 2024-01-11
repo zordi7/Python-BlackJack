08782167dd4bea414ae3be711be31459924da56d
zordi7 <156319997+zordi7@users.noreply.github.com>
2024-01-11 20:45:20 +0100

    Dati.py

---
+++ Dati.py
@@ -0,0 +1,15 @@
+def ChiediIntero(prompt):
+    intero = int(input(prompt))
+    return intero
+
+def ChiediReale(prompt):
+    reale = float(input(prompt))
+    return reale
+
+def ChiediParola(prompt):
+    parola = str(input(prompt))
+    return parola
+
+def spazi(n):
+    for i in range(n):
+        print()
